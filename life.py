b575b833e3931f6aa4a4d3e7056c19b83df4397c
Jacks <52548283+JacksMuch@users.noreply.github.com>
2019-08-14 13:53:49 -0400

    created base file

---
+++ life.py
@@ -0,0 +1,12 @@
+def doorstep():
+  print("Oh, it looks like you left your life behind.")
+  print("Want me to pick it up? Just kidding, you're my slave!")
+  
+ def point():
+  print("The point of life means to be pointless.")
+  
+ def useful():
+  print("No, I am not useful. However you can check out my devs github! github.com/jacksmuch")
+
+ def what():
+  print("...")
